6e6b5d0faf4c631fe8611aa75a81e589ca04bf08
Alexandre <em_alexandre@yahoo.co.jp>
2022-09-16 15:59:02 +0200

    Add github deploying action

--- src/screens/Projects/index.tsx
+++ src/screens/Projects/index.tsx
@@ -1,15 +1,19 @@
 import {Typography} from "@mui/material";
 import {blueGrey} from "@mui/material/colors";
+import Footer from "../../components/Footer";
 import ProjectsGallery from "../../components/ProjectsGallery";
 
 
 export default function Projects() {
   return (
-    <div style={{ padding: 25 }}>
-      <Typography variant="h4" style={{ fontWeight: '700', marginBottom: 25 }} color={blueGrey[800]}>
-        My Projects
-      </Typography>
-      <ProjectsGallery />
+    <div>
+      <div style={{ padding: 25 }}>
+        <Typography variant="h4" style={{ fontWeight: '700', marginBottom: 25 }} color={blueGrey[800]}>
+          My Projects
+        </Typography>
+        <ProjectsGallery />
+      </div>
+      <Footer />
     </div>
   );
 };
